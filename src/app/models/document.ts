8f6b299ebb3c9d7301deab43abe37db20d952eaa
Miccighel <miccighel@gmail.com>
2019-12-06 18:54:39 +0100

    A lot of controls and items added.

---
+++ src/app/models/document.ts
@@ -0,0 +1,11 @@
+export class Document {
+
+  number: number;
+  text: string;
+
+  constructor(number, text) {
+    this.number=number;
+    this.text=text;
+  }
+
+}
---
+++ src/app/models/hit.ts
@@ -0,0 +1,15 @@
+export interface Hit {
+
+  // FIELDS TO PARSE FROM JSON
+
+  unit_id: string
+  token_input: string
+  token_output: string
+
+  document_1: string
+  document_2: string
+  document_3: string
+  high: number
+  low: number
+
+}
