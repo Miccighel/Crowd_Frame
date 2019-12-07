c3b8e049c3ed017bc86ede0e44385d61bbedf9f0
Michael Soprano <miccighel@gmail.com>
2019-12-07 21:00:00 +0100

    Improvements

--- src/app/models/hit.ts
+++ src/app/models/hit.ts
@@ -2,10 +2,13 @@ export interface Hit {
 
   // FIELDS TO PARSE FROM JSON
 
+  hit_number
+
   unit_id: string
   token_input: string
   token_output: string
 
+  documents_number: number
   document_1: string
   document_2: string
   document_3: string
--- src/app/skeleton/skeleton.component.ts
+++ src/app/skeleton/skeleton.component.ts
@@ -1,22 +1,35 @@
-import {Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit, ViewChild} from '@angular/core';
 import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms'
 import {HitsService,} from "../services/hits.service";
 import {S3Service} from "../services/s3.service";
 import {ConfigService} from "../services/config.service";
-import { NgxUiLoaderService } from 'ngx-ui-loader';
+import {NgxUiLoaderService} from 'ngx-ui-loader';
 import {Document} from "../models/document";
+import {Hit} from "../models/hit";
+import {MatStepper} from "@angular/material/stepper";
 
 @Component({
   templateUrl: './skeleton.component.html',
-  styleUrls: ['./skeleton.component.scss']
+  styleUrls: ['./skeleton.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 
-export class Skeleton implements OnInit{
+export class Skeleton {
+
+  @ViewChild('stepper', {static:false}) stepper;
+
+  changeDetector: ChangeDetectorRef;
 
   ngxService: NgxUiLoaderService;
   configService: ConfigService;
   hitsService: HitsService;
   S3Service: S3Service;
+
+  taskStarted: boolean;
+  taskCompleted: boolean;
+  taskSuccessful: boolean;
+
+  hit: Hit;
 
   crowdForm: FormGroup;
   tokenInput: FormControl;
@@ -29,26 +42,34 @@ export class Skeleton implements OnInit{
   documents: Array<Document>;
 
   constructor(
-    formBuilder: FormBuilder,
+    changeDetector: ChangeDetectorRef,
     ngxService: NgxUiLoaderService,
     configService: ConfigService,
     hitsService: HitsService,
     S3Service: S3Service,
+    formBuilder: FormBuilder,
   ) {
+
+    this.changeDetector = changeDetector;
 
     this.configService = configService;
     this.ngxService = ngxService;
     this.hitsService = hitsService;
     this.S3Service = S3Service;
 
+    this.taskStarted = false;
+    this.taskCompleted = false;
+    this.taskSuccessful = false;
+
+
     this.tokenInput = new FormControl('token_input_1', [Validators.required], this.hitsService.validateTokenInput(this.configService.environment.hitsPath));
-    this.profession = new FormControl('', [Validators.required, Validators.minLength(5)]);
-    this.about = new FormControl('', [Validators.required, Validators.minLength(20)]);
-    this.age = new FormControl('', [Validators.required]);
+    this.profession = new FormControl('This is the profession', [Validators.required, Validators.minLength(5)]);
+    this.about = new FormControl('This is the about', [Validators.required, Validators.minLength(10)]);
+    this.age = new FormControl('20+', [Validators.required]);
     this.opinions = new FormArray([
-      new FormControl('', [Validators.required]),
-      new FormControl('', [Validators.required]),
-      new FormControl('', [Validators.required]),
+      new FormControl('This is the first opinion', [Validators.required]),
+      new FormControl('This is the second opinion', [Validators.required]),
+      new FormControl('This is the third opinion', [Validators.required]),
     ]);
 
     this.crowdForm = formBuilder.group({
@@ -61,33 +82,57 @@ export class Skeleton implements OnInit{
 
   }
 
-  ngOnInit() {}
-
-  public checkError = (field: string, key: string): boolean => {
+  public checkError(field: string, key: string): boolean {
     return this.crowdForm.get(field).hasError(key);
-  };
+  }
 
   public getErrorMessage(field: string, key: string): string {
     return this.crowdForm.get(field).errors[key]
   }
 
-  public loadHits() {
+  public loadHit(): void {
     this.hitsService.loadJSON(this.configService.environment.hitsPath).subscribe(
       hits => {
         this.ngxService.start();
-        let hit = null;
         for (let currentHit of hits) {
           if (this.tokenInput.value === currentHit.token_input) {
-            hit = currentHit;
+            this.hit = currentHit;
           }
         }
         this.documents = new Array<Document>();
-        this.S3Service.retrieveDocument(hit.document_1).subscribe(document => this.documentsNumber = this.documents.push(document));
-        this.S3Service.retrieveDocument(hit.document_2).subscribe(document => this.documentsNumber = this.documents.push(document));
-        this.S3Service.retrieveDocument(hit.document_3).subscribe(document => this.documentsNumber = this.documents.push(document));
+        this.S3Service.retrieveDocument(this.hit.document_1).subscribe(document => this.documents.push(document));
+        this.S3Service.retrieveDocument(this.hit.document_2).subscribe(document => this.documents.push(document));
+        this.S3Service.retrieveDocument(this.hit.document_3).subscribe(document => this.documents.push(document));
         this.ngxService.stop();
+        this.taskStarted = true;
+        this.changeDetector.detectChanges();
       }
     );
+  }
+
+  public performFinalCheck() {
+
+    this.ngxService.start();
+
+    this.taskCompleted = true;
+
+    console.log("---------- CHECKING QUESTIONNAIRE ----------");
+    console.log(`AGE: ${this.crowdForm.value}`);
+    console.log(this.crowdForm.value["age"]);
+    console.log(`PROFESSION: ${this.crowdForm.value}`);
+    console.log(this.crowdForm.value["profession"]);
+    console.log(`ABOUT: ${this.crowdForm.value}`);
+    console.log(this.crowdForm.value["about"]);
+
+    console.log("---------- CHECKING DOCUMENTS ----------");
+
+
+    this.taskSuccessful = true;
+
+    this.ngxService.stop();
+
+    this.changeDetector.detectChanges();
+
   }
 
 }
