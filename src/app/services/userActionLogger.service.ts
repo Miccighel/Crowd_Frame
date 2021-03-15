07cf3ec94979fd0b13e2e822da7d53a4e8d7e9e0
bomb697 <50354770+bomb697@users.noreply.github.com>
2021-03-15 19:37:32 +0100

    Class refactoring

--- src/app/services/userActionLogger.service.ts
+++ src/app/services/userActionLogger.service.ts
@@ -9,59 +9,90 @@ import {Injectable} from "@angular/core";
   providedIn: 'root',
 })
 export class ActionLogger {
-  private logSeq: number;
-  private readonly initTime: Date;
+  private sequenceNumber: number;
+  private readonly initTime: number;
   private workerID: string;
   private taskName: string;
   private batchName: string;
   private http: HttpClient;
   private init = false;  // This variable is only needed to not send double request on ngInit
 
+  /**
+   * Default constructor
+   * Initialize the sequence number to 0 and the initialization time of the logger
+   */
   constructor(){
-    this.logSeq = 0;
+    this.sequenceNumber = 0;
+    this.initTime = new Date().getTime()/1000;
   }
 
-  logAction(type, details){
-    let payload = {
+  /**
+   * Construct a payload for the http request from given details
+   * @param type of the action that generated the log
+   * @param details of the event
+   */
+  buildPayload(type: string, details: object){
+    return {
       type : type,
       worker : this.workerID,
       task : this.taskName,
       batch : this.batchName,
-      timestamp : new Date(),
-      sequence : this.logSeq++,
+      action_time : new Date().getTime(),
+      sequence : this.sequenceNumber++,
       details: details
     }
-
-    console.log(payload)
-
-    // this.http.post(
-    //   'https://8vd1uyg771.execute-api.us-east-1.amazonaws.com/logger/log',
-    //   payload,
-    //   {
-    //     responseType: 'text',
-    //     headers: new HttpHeaders()
-    //       .set('content-type', 'text/plain')
-    //   }).subscribe()
   }
 
-  logInit(workerID, taskName, batchName, http){
-    this.workerID = workerID;
-    this.taskName = taskName;
-    this.batchName = batchName;
-    this.http = http;
+  /**
+   * Send the POST request to the gateway to log the event
+   * @param type of the action that generated the log
+   * @param details of the event
+   */
+  log(type: string, details: object) {
+    let payload = this.buildPayload(type, details)
 
-    let details = {
-      screen_size : {width : window.screen.width, height : window.screen.height},
-      window_size : {width : window.innerWidth, height: window.innerHeight}
-    }
+    this.http.post(
+      'https://8vd1uyg771.execute-api.us-east-1.amazonaws.com/logger/log',
+      payload,
+      {
+        responseType: 'text',
+        headers: new HttpHeaders()
+          .set('content-type', 'text/plain')
+      }).subscribe()
+  }
 
+  /**
+   * This function is called once to log the context information of the session
+   */
+  logContext(){
+    let payload = this.buildPayload('context', null)
+
+    this.http.post('https://8vd1uyg771.execute-api.us-east-1.amazonaws.com/logger/stats', payload).subscribe(data => console.log(data))
+  }
+
+  /**
+   * First log function called. Sets session-bounded variables, calls logContext and logs basic information about the session
+   * @param workerID
+   * @param taskName
+   * @param batchName
+   * @param http client initialized by the skeleton
+   */
+  logInit(workerID: string, taskName: string, batchName: string, http: HttpClient){
     if(!this.init) {
-      this.init = true;
-      this.logAction('init', details)
-    }
-  }
+      this.init = true
 
-  onClick(event){
-    this.logAction('click', event)
+      this.http = http;
+      this.workerID = workerID;
+      this.taskName = taskName;
+      this.batchName = batchName;
+
+      let details = {
+        screen_size : {width : window.screen.width, height : window.screen.height},
+        window_size : {width : window.innerWidth, height: window.innerHeight}
+      }
+
+      this.logContext()
+      this.log('init', details)
+    }
   }
 }
