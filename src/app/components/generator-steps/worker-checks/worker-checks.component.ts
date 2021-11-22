71e183b51a6433334724947fad5ea379fc4550d8
Michael Soprano <miccighel@gmail.com>
2021-11-22 15:35:33 +0100

    Random CSS Fixes, styling for the two new fields in Step #7.
    The whole CSS styling must be refactored in the future.

--- src/app/components/generator-steps/worker-checks/worker-checks.component.ts
+++ src/app/components/generator-steps/worker-checks/worker-checks.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output, ViewEncapsulation} from '@angular/core';
 import {MatChipInputEvent} from "@angular/material/chips";
 import {FormBuilder, FormGroup} from "@angular/forms";
 import {COMMA, ENTER} from "@angular/cdk/keycodes";
