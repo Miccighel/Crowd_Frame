ec3d031a77f9ec703f6b674bcab28bb9ee010a73
Michael Soprano <miccighel@gmail.com>
2022-02-25 16:35:24 +0100

    Handling nested questionnaires - 1

--- src/app/components/skeleton/questionnaire/questionnaire.component.ts
+++ src/app/components/skeleton/questionnaire/questionnaire.component.ts
@@ -1,5 +1,5 @@
 import {ChangeDetectorRef, Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {Questionnaire} from "../../../models/questionnaire";
+import {Mapping, Question, Questionnaire} from "../../../models/questionnaire";
 import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {SectionService} from "../../../services/section.service";
 import {UtilsService} from "../../../services/utils.service";
@@ -49,99 +49,48 @@ export class QuestionnaireComponent implements OnInit {
     ngOnInit(): void {
         this.questionnaire = this.task.questionnaires[this.questionnaireIndex];
         if (this.questionnaire.type == "standard" || this.questionnaire.type == "likert") {
-            let controlsConfig = {};
-            for (let indexQuestion = 0; indexQuestion < this.questionnaire.questions.length; indexQuestion++) {
-                let currentQuestion = this.questionnaire.questions[indexQuestion]
-                if (currentQuestion.type != 'section') {
-                    let controlName = `${currentQuestion.name}`
-                    let validators = []
-                    if (currentQuestion.required) validators = [Validators.required]
-                    if (currentQuestion.type == 'number') validators.concat([Validators.min(0), Validators.max(100)])
-                    if (currentQuestion.type == 'email') validators.push(Validators.email)
-                    controlsConfig[`${controlName}_answer`] = new FormControl('', validators)
-                    if (currentQuestion.freeText) controlsConfig[`${controlName}_free_text`] = new FormControl('')
-                }
-                if (currentQuestion.questions) {
-                    for (let indexQuestionSub = 0; indexQuestionSub < currentQuestion.questions.length; indexQuestionSub++) {
-                        let currentQuestionSub = currentQuestion.questions[indexQuestionSub]
-                        if (currentQuestionSub.type != 'section') {
-                            let controlNameSub = `${currentQuestion.nameFull}_${currentQuestionSub.name}`
-                            let validators = []
-                            if (currentQuestionSub.required) validators = [Validators.required]
-                            if (currentQuestionSub.type == 'number') validators.concat([Validators.min(0), Validators.max(100)])
-                            if (currentQuestionSub.type == 'email') validators.push(Validators.email)
-                            controlsConfig[`${controlNameSub}_answer`] = new FormControl('', validators)
-                            if (currentQuestionSub.freeText) controlsConfig[`${controlNameSub}_free_text`] = new FormControl('')
-                        }
-                        if (currentQuestionSub.questions) {
-                            for (let indexQuestionSubSub = 0; indexQuestionSubSub < currentQuestionSub.questions.length; indexQuestionSubSub++) {
-                                let currentQuestionSubSub = currentQuestionSub.questions[indexQuestionSubSub]
-                                if (currentQuestionSubSub.type != 'section') {
-                                    let controlNameSubSub = `${currentQuestionSub.nameFull}_${currentQuestionSubSub.name}`
-                                    let validators = []
-                                    if (currentQuestionSubSub.required) validators = [Validators.required]
-                                    if (currentQuestionSubSub.type == 'number') validators.concat([Validators.min(0), Validators.max(100)])
-                                    if (currentQuestionSubSub.type == 'email') validators.push(Validators.email)
-                                    controlsConfig[`${controlNameSubSub}_answer`] = new FormControl('', validators)
-                                    if (currentQuestionSubSub.freeText) controlsConfig[`${controlNameSubSub}_free_text`] = new FormControl('')
-                                }
-                                if (currentQuestionSubSub.questions) {
-                                    for (let indexQuestionSubSubSub = 0; indexQuestionSubSubSub < currentQuestionSubSub.questions.length; indexQuestionSubSubSub++) {
-                                        let currentQuestionSubSubSub = currentQuestionSubSub.questions[indexQuestionSubSubSub]
-                                        if (currentQuestionSubSubSub.type != 'section') {
-                                            let controlNameSubSubSub = `${currentQuestionSubSub.nameFull}_${currentQuestionSubSubSub.name}`
-                                            let validators = []
-                                            if (currentQuestionSubSubSub.required) validators = [Validators.required]
-                                            if (currentQuestionSubSubSub.type == 'number') validators.concat([Validators.min(0), Validators.max(100)])
-                                            if (currentQuestionSubSubSub.type == 'email') validators.push(Validators.email)
-                                            controlsConfig[`${controlNameSubSubSub}_answer`] = new FormControl('', validators)
-                                            if (currentQuestionSubSubSub.freeText) controlsConfig[`${controlNameSubSubSub}_free_text`] = new FormControl('')
-                                        }
-                                        if (currentQuestionSubSubSub.questions) {
-                                            for (let indexQuestionSubSubSubSub = 0; indexQuestionSubSubSubSub < currentQuestionSubSubSub.questions.length; indexQuestionSubSubSubSub++) {
-                                                let currentQuestionSubSubSubSub = currentQuestionSubSubSub.questions[indexQuestionSubSubSubSub]
-                                                if (currentQuestionSubSubSubSub.type != 'section') {
-                                                    let controlNameSubSubSubSub = `${currentQuestionSubSubSub.nameFull}_${currentQuestionSubSubSubSub.name}`
-                                                    let validators = []
-                                                    if (currentQuestionSubSubSubSub.required) validators = [Validators.required]
-                                                    if (currentQuestionSubSubSubSub.type == 'number') validators.concat([Validators.min(0), Validators.max(100)])
-                                                    if (currentQuestionSubSubSubSub.type == 'email') validators.push(Validators.email)
-                                                    controlsConfig[`${controlNameSubSubSubSub}_answer`] = new FormControl('', validators)
-                                                    if (currentQuestionSubSubSubSub.freeText) controlsConfig[`${controlNameSubSubSubSub}_free_text`] = new FormControl('')
-                                                }
-                                                if (currentQuestionSubSubSubSub.questions) {
-                                                    for (let indexQuestionSubSubSubSubSub = 0; indexQuestionSubSubSubSubSub < currentQuestionSubSubSubSub.questions.length; indexQuestionSubSubSubSubSub++) {
-                                                        let currentQuestionSubSubSubSubSub = currentQuestionSubSubSubSub.questions[indexQuestionSubSubSubSubSub]
-                                                        if (currentQuestionSubSubSubSubSub.type != 'section') {
-                                                            let controlNameSubSubSubSubSub = `${currentQuestionSubSubSubSub.nameFull}_${currentQuestionSubSubSubSubSub.name}`
-                                                            let validators = []
-                                                            if (currentQuestionSubSubSubSubSub.required) validators = [Validators.required]
-                                                            if (currentQuestionSubSubSubSubSub.type == 'number') validators.concat([Validators.min(0), Validators.max(100)])
-                                                            if (currentQuestionSubSubSubSubSub.type == 'email') validators.push(Validators.email)
-                                                            controlsConfig[`${controlNameSubSubSubSubSub}_answer`] = new FormControl('', validators)
-                                                            if (currentQuestionSubSubSubSubSub.freeText) controlsConfig[`${controlNameSubSubSubSubSub}_free_text`] = new FormControl('')
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            this.questionnaireForm = this.formBuilder.group(controlsConfig)
+            this.exploreSubQuestions(this.questionnaire.questions)
+
         } else {
             /* If the questionnaire is a CRT one it means that it has only one question where the answer must be a number between 0 and 100 chosen by user; required, max and min validators are needed */
             let controlsConfig = {};
             for (let index_question = 0; index_question < this.questionnaire.questions.length; index_question++) controlsConfig[`${this.questionnaire.questions[index_question].name}`] = new FormControl('', [Validators.max(100), Validators.min(0), Validators.required])
             this.questionnaireForm = this.formBuilder.group(controlsConfig)
         }
+        this.formEmitter.emit({
+            "form": this.questionnaireForm,
+            "action": null
+        })
     }
 
-    public handleQuestionnaireCompletion(action: string){
+    public exploreSubQuestions(obj: Array<Question>) {
+        Object.entries(obj).forEach(
+            ([key, value]) => {
+                if ('questions' in value) {
+                    if (value['questions']) {
+                        console.log(value.name)
+                        // Perform things
+                        // let currentQuestion = this.questionnaire.questions[indexQuestion]
+                        // if (currentQuestion.type != 'section') {
+                        //     let controlName = `${currentQuestion.name}`
+                        //     let validators = []
+                        //     if (currentQuestion.required) validators = [Validators.required]
+                        //     if (currentQuestion.type == 'number') validators.concat([Validators.min(0), Validators.max(100)])
+                        //     if (currentQuestion.type == 'email') validators.push(Validators.email)
+                        //     controlsConfig[`${controlName}_answer`] = new FormControl('', validators)
+                        //     if (currentQuestion.freeText) controlsConfig[`${controlName}_free_text`] = new FormControl('')
+                        // }
+                        this.exploreSubQuestions(value['questions'])
+                    }
+                } else {
+                    // Perform things
+                    console.log(value.name)
+                }
+            }
+        );
+    }
+
+    public handleQuestionnaireCompletion(action: string) {
         this.formEmitter.emit({
             "form": this.questionnaireForm,
             "action": action
--- src/app/models/questionnaire.ts
+++ src/app/models/questionnaire.ts
@@ -22,47 +22,13 @@ export class Questionnaire {
         this.allow_back = data['allow_back'] ? data["allow_back"] : false;
         this.type = data["type"];
         this.questions = new Array<Question>();
-        if(data["questions"])
-            for (let index = 0; index < data["questions"].length; index++) this.questions.push(new Question(index, data["questions"][index]))
+        this.questions = data['questions'] ? data['questions'].map((item, index) => new Question(index, item)) : [];
         if (data['mapping']) {
             this.mappings = new Array<Mapping>();
             for (let index = 0; index < data["mapping"].length; index++) this.mappings.push(new Mapping(index, data["mapping"][index]))
         }
-        for (let indexQuestion = 0; indexQuestion < this.questions.length; indexQuestion++) {
-            let currentQuestion = this.questions[indexQuestion]
-            currentQuestion.nameFull = currentQuestion.name
-            if (currentQuestion.questions) {
-                for (let indexQuestionSub = 0; indexQuestionSub < currentQuestion.questions.length; indexQuestionSub++) {
-                    let currentQuestionSub = currentQuestion.questions[indexQuestionSub]
-                    currentQuestionSub.nameFull = `${currentQuestion.nameFull}_${currentQuestionSub.name}`
-                    if (currentQuestionSub.questions) {
-                        for (let indexQuestionSubSub = 0; indexQuestionSubSub < currentQuestionSub.questions.length; indexQuestionSubSub++) {
-                            let currentQuestionSubSub = currentQuestionSub.questions[indexQuestionSubSub]
-                            currentQuestionSubSub.nameFull = `${currentQuestionSub.nameFull}_${currentQuestionSubSub.name}`
-                            if (currentQuestionSubSub.questions) {
-                                for (let indexQuestionSubSubSub = 0; indexQuestionSubSubSub < currentQuestionSubSub.questions.length; indexQuestionSubSubSub++) {
-                                    let currentQuestionSubSubSub = currentQuestionSubSub.questions[indexQuestionSubSubSub]
-                                    currentQuestionSubSubSub.nameFull = `${currentQuestionSubSub.nameFull}_${currentQuestionSubSubSub.name}`
-                                    if (currentQuestionSubSubSub.questions) {
-                                        for (let indexQuestionSubSubSubSub = 0; indexQuestionSubSubSubSub < currentQuestionSubSubSub.questions.length; indexQuestionSubSubSubSub++) {
-                                            let currentQuestionSubSubSubSub = currentQuestionSubSubSub.questions[indexQuestionSubSubSubSub]
-                                            currentQuestionSubSubSubSub.nameFull = `${currentQuestionSubSubSub.nameFull}_${currentQuestionSubSubSubSub.name}`
-                                            if (currentQuestionSubSubSubSub.questions) {
-                                                for (let indexQuestionSubSubSubSubSub = 0; indexQuestionSubSubSubSubSub < currentQuestionSubSubSubSub.questions.length; indexQuestionSubSubSubSubSub++) {
-                                                    let currentQuestionSubSubSubSubSub = currentQuestionSubSubSubSub.questions[indexQuestionSubSubSubSubSub]
-                                                    currentQuestionSubSubSubSubSub.nameFull = `${currentQuestionSubSubSubSub.nameFull}_${currentQuestionSubSubSubSubSub.name}`
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
     }
+
 }
 
 export class Question {
@@ -72,6 +38,8 @@ export class Question {
 
     name: string
     type: string
+    repeat?: boolean
+    times?: number
     nameFull?: string
     required?: boolean
     text?: string
@@ -92,6 +60,8 @@ export class Question {
         this.type = data['type']
         this.nameFull = null
         this.text = data['text'] ? data["text"] : null
+        this.repeat = data['repeat'] ? data["repeat"] : null
+        this.times = data['times'] ? parseInt(data["times"]) : null
         this.required = data['required']
         if (data['answers']) {
             this.answers = new Array<string>();
@@ -99,7 +69,7 @@ export class Question {
         }
         if (data['questions']) {
             this.questions = new Array<Question>();
-            for (let index = 0; index < data["questions"].length; index++) this.questions.push(new Question(index, data["questions"][index]))
+            this.questions.push(new Question(index, data["questions"][index]))
         }
         this.detail = data['detail'] ? new Detail(data["detail"]) : null;
         this.showDetail = data['show_detail'] ? data['show_detail'] : false;
